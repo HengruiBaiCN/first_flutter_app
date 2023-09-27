46c4f0283e1bf6eafca4d9725549ff17816f57c7
Hengrui Bai <u6968198@anu.edu.au>
2023-09-27 16:54:59 +1000

    update configuration

--- lib/config.dart
+++ lib/config.dart
@@ -2,7 +2,10 @@ class Config {
   static String key = "ck_5548abde185d8a83fa7781e7c6bce5459f52616c";
   static String secret = "cs_6ae6830cba04c992ed7bf63ed36f5d1015661015";
   static String token = "";
-  static String url = "https://dilkara.com.au/wc-api/v3/";
+  static String url = "https://dilkara.com.au/wp-json/wc/v3/";
   static String custormerUrl = "customers/";
   static String categoryUrl = "products/categories/";
+
+  static String productUrl = "products/";
+  // static String
 }
